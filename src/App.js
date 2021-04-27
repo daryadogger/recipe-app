10447ed714d5759b8d04af2b815cd518acd9e5ea
daryadogger <zhihareva.d@mail.ru>
2021-04-27 21:14:12 +0700

    Made main component with list and button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import {Accordion, Button, Card, Container, ListGroup} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {recipes} from './mocks/recipes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="pt-5">
+      <h1 className="mb-5 text-center">It's a recipes app! You're welcome!</h1>
+
+      <Accordion className="mb-4">
+
+        {recipes.map((recipe) => 
+          <Card id={recipe.id} key={`recipe-${recipe.id}`}>
+
+            <Accordion.Toggle as={Card.Header} eventKey={recipe.id + 1} className="d-flex justify-content-between">
+              <p className="mb-0">{recipe.title}</p>
+              <p className="mb-0">
+                {recipe.time + ` hours`}</p>
+            </Accordion.Toggle>
+
+            <Accordion.Collapse eventKey={recipe.id + 1}>
+              <ListGroup>
+                <ListGroup.Item>{recipe.ingridients}</ListGroup.Item>
+              </ListGroup>
+            </Accordion.Collapse>
+            
+          </Card>
+        )}
+
+      </Accordion>
+
+      <Button variant="info" size="lg">Add recipe</Button>
+
+    </Container>
+
   );
 }
 
---
+++ src/mocks/recipes.js
@@ -0,0 +1,20 @@
+export const recipes = [
+  {
+    id: 0,
+    title: `Beef & Broccoli`,
+    time: 2,
+    ingridients: `Beef, Broccoli, Cream, Salt, Paper`
+  },
+  {
+    id: 1,
+    title: `Banana milkshake`,
+    time: 1,
+    ingridients: `Bananas, Milk, Ice cream`
+  },
+  {
+    id: 6,
+    title: `Pizza`,
+    time: 3,
+    ingridients: `Dough, Fresh basil leaves, Pinch red pepper flakes, Mozzarella`
+  },
+];
