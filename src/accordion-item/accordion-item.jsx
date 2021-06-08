ef4634d6b3a54fd13f7355aad037e3786a448b87
Darya Zhihareva <zhihareva.d@mail.ru>
2021-06-08 14:25:37 +0700

    Added spaces for accordion-item

--- src/accordion-item/accordion-item.jsx
+++ src/accordion-item/accordion-item.jsx
@@ -4,30 +4,30 @@ import {getSplitedString} from "../functions/get-splited-string";
 import AccordionItemView from "./accordion-item-view";
 
 function AccordionItem(props) {
-  const {recipe, setRecipes, id} = props;
-  
-  const ingredients = getSplitedString(recipe.ingredients, `,`);
-
-  const [isShowEditModal, setIsShowEditModal] = useState(false);
-
-  const handleBtnEditClick = useCallback(() => {
-    setIsShowEditModal(!isShowEditModal);
-  }, [isShowEditModal]);
-
-  const handleBtnDeleteClick = (id) => {
-    let _recipes = JSON.parse(localStorage.getItem('recipes')).slice();
-    let recipeIndex = _recipes.findIndex(recipe => recipe.id === id);
+    const {recipe, setRecipes, id} = props;
     
-    _recipes.splice(recipeIndex, 1);
-    setRecipes(_recipes);
-  };
+    const ingredients = getSplitedString(recipe.ingredients, `,`);
 
-  return(
+    const [isShowEditModal, setIsShowEditModal] = useState(false);
 
-    <AccordionItemView id={id} ingredients={ingredients} recipe={recipe} onBtnEditClick={handleBtnEditClick} onBtnDeleteClick={handleBtnDeleteClick}
-      setRecipes={setRecipes} isShowEditModal={isShowEditModal} setIsShowEditModal={setIsShowEditModal} />
+    const handleBtnEditClick = useCallback(() => {
+        setIsShowEditModal(!isShowEditModal);
+    }, [isShowEditModal]);
 
-  );
+    const handleBtnDeleteClick = (id) => {
+        let _recipes = JSON.parse(localStorage.getItem('recipes')).slice();
+        let recipeIndex = _recipes.findIndex(recipe => recipe.id === id);
+        
+        _recipes.splice(recipeIndex, 1);
+        setRecipes(_recipes);
+    };
+
+    return(
+
+        <AccordionItemView id={id} ingredients={ingredients} recipe={recipe} onBtnEditClick={handleBtnEditClick} onBtnDeleteClick={handleBtnDeleteClick}
+        setRecipes={setRecipes} isShowEditModal={isShowEditModal} setIsShowEditModal={setIsShowEditModal} />
+
+    );
 };
 
 export default AccordionItem;
