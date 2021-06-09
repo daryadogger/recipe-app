afec157c222dfe01d390d14a3d66423c65ba7bf0
Darya Zhihareva <zhihareva.d@mail.ru>
2021-06-09 12:40:38 +0700

    Added configs for jest

--- src/app/app.test.js
+++ src/app/app.test.js
@@ -3,9 +3,28 @@ import {render} from '@testing-library/react';
 import App from './app.jsx';
 
 it(`Отображает компонент App`, () => {
+  const recipes = [
+    {
+      id: 0,
+      name: `Beef & Broccoli`,
+      ingredients: `Beef, Broccoli, Cream, Salt, Paper`
+    },
+    {
+      id: 1,
+      name: `Banana milkshake`,
+      ingredients: `Bananas, Milk, Ice cream`
+    },
+    {
+      id: 2,
+      name: `Pizza`,
+      ingredients: `Dough, Fresh basil leaves, Pinch red pepper flakes, Mozzarella`
+    },
+  ];
+
   const {getByText} = render(
-      <App />
+      <App recipesMocks={recipes} />
   );
 
-  expect(getByText(/It's a recipes app! You're welcome!/i)).toBeInTheDocument();
+  const title = getByText(/It's a recipes app! You're welcome!/i);
+  expect(title).toBeInTheDocument();
 });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './report-web-vitals';
 import {recipes} from './mocks/recipes';
 import App from './app/app.jsx';
 
